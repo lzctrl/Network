0b055c9a627b2192aa70491a7f051bdeaa40200f
MLozyy <MLozovyywd@gmail.com>
2020-05-20 23:49:00 -0400

    Initial Commit

---
+++ Network/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Network
+//
+//  Created by Mikhail Lozovyy on 5/20/20.
+//  Copyright © 2020 LzCtrl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
